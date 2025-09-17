d44e71c7dc7c222d98b6d45b9ea7e5fa352a4357
Pranshu0808 <pranshusharma789123@gmail.com>
2025-09-18 03:36:43 +0530

    Time: 147 ms (54.57%) | Memory: 162.8 MB (84.21%) - LeetSync

---
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -0,0 +1,29 @@
+
+
+class FoodRatings {
+    unordered_map<string, pair<string,int>> foodInfo;
+    unordered_map<string, set<pair<int,string>>> cuisineFoods; 
+public:
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        for (int i = 0; i < n; i++) {
+            foodInfo[foods[i]] = {cuisines[i], ratings[i]};
+            cuisineFoods[cuisines[i]].insert({-ratings[i], foods[i]});
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        auto [cuisine, oldRating] = foodInfo[food];
+
+        cuisineFoods[cuisine].erase({-oldRating, food});
+       
+        cuisineFoods[cuisine].insert({-newRating, food});
+       
+        foodInfo[food].second = newRating;
+    }
+    
+    string highestRated(string cuisine) {
+       
+        return cuisineFoods[cuisine].begin()->second;
+    }
+};
