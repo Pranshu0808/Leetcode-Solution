adea395cbdc8bc7883a06133901e20f72ff212a2
Pranshu0808 <pranshusharma789123@gmail.com>
2025-12-12 22:20:27 +0530

    Time: 2 ms (65.05%) | Memory: 14.9 MB (68.42%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int a = 0 , b = 0;
+        for(int i = 0 ; i < s.size(); ++i){
+            if(s[i] == 'a'){
+                a++;
+            }
+            else{
+                b++;
+            }
+        }
+        return abs(a-b);
+    }
+};
