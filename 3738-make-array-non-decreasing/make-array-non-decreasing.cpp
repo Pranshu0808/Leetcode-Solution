1a8e456f977b04e7044e184cbdda3446fc4593d9
Pranshu0808 <pranshusharma789123@gmail.com>
2025-12-15 05:13:32 +0530

    Time: 27 ms (15.75%) | Memory: 211.7 MB (15.30%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.cpp
@@ -1,22 +1,44 @@
+// class Solution {
+// public:
+//     int maximumPossibleSize(vector<int>& arr) {
+//         int n = arr.size();
+//         stack<int> st;
+//         for(int i = 0 ; i < n; ++i){
+//            if(st.empty()){
+//                 st.push(arr[i]);
+//            }
+//            else if(!st.empty() &&  st.top() <= arr[i]){
+//                 st.push(arr[i]);
+//            }
+//         }
+//         int ct = 0;
+//         while(!st.empty()){
+//             ct++;
+//             st.pop();
+//         }
+//         return ct;
+
+//     }
+// };
+
 class Solution {
 public:
-    int maximumPossibleSize(vector<int>& arr) {
-        int n = arr.size();
+    int maximumPossibleSize(vector<int>& nums) {
         stack<int> st;
-        for(int i = 0 ; i < n; ++i){
-           if(st.empty()){
-                st.push(arr[i]);
-           }
-           else if(!st.empty() &&  st.top() <= arr[i]){
-                st.push(arr[i]);
-           }
-        }
-        int ct = 0;
-        while(!st.empty()){
-            ct++;
-            st.pop();
-        }
-        return ct;
+        int n = nums.size();
 
+        for(int i = 0; i < n; i++){
+            int x = nums[i];
+
+            
+            while(!st.empty() && st.top() > x){
+                x = st.top(); 
+                st.pop();
+            }
+
+            st.push(x);
+        }
+
+        return st.size();
     }
 };
