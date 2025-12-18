ae9aa2b685673875c53b73067dd5b433a907c3cd
Pranshu0808 <pranshusharma789123@gmail.com>
2025-12-18 14:08:45 +0530

    Time: 0 ms (100.00%) | Memory: 51.7 MB (92.27%) - LeetSync

---
+++ 2402-maximum-xor-after-operations/maximum-xor-after-operations.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maximumXOR(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0, n = nums.size(); i < n; ++i){ res |= nums[i];
+        }
+        return res;
+    }
+};
