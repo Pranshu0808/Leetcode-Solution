50a9720c882fa8a9a2e0ea05703167885c4a1ac4
Pranshu0808 <pranshusharma789123@gmail.com>
2025-12-24 04:53:03 +0530

    Time: 12 ms (29.49%) | Memory: 23.1 MB (11.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,34 +1,60 @@
-class Solution {
+// class Solution {
 
 
 
-public:
+// public:
 
 
-        long long func(vector<int> piles , int mid){
-            int n = piles.size();
-            long long tt = 0;
-            for(int i = 0; i < n; ++i){
-                tt += ceil(piles[i]*1.0/mid);
-            }
-            return tt;
-        }
+//         long long func(vector<int> piles , int mid){
+//             int n = piles.size();
+//             long long tt = 0;
+//             for(int i = 0; i < n; ++i){
+//                 tt += ceil(piles[i]*1.0/mid);
+//             }
+//             return tt;
+//         }
 
-    int minEatingSpeed(vector<int>& piles, int h){
+//     int minEatingSpeed(vector<int>& piles, int h){
         
-        int lo = 1, hi = *max_element(piles.begin() , piles.end()) , ans = INT_MAX;;
-        while(lo <= hi){
-            int mid = (lo+ hi )/2;
-            long long reqtime = func(piles , mid);
-            if(reqtime <= h){
-                ans = mid;
-                hi = mid-1;
+//         int lo = 1, hi = *max_element(piles.begin() , piles.end()) , ans = INT_MAX;;
+//         while(lo <= hi){
+//             int mid = (lo+ hi )/2;
+//             long long reqtime = func(piles , mid);
+//             if(reqtime <= h){
+//                 ans = mid;
+//                 hi = mid-1;
+//             }
+//             else{
+//                 lo = mid + 1;
+//             }
+//         }
+//         return lo;
+
+//     }
+// };
+
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int ans=-1;
+        sort(piles.begin(),piles.end());
+        int lo=1,hi=piles[n-1];
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            long long cnt=0;
+            for(int i=0;i<n;i++){
+                    cnt+=ceil((double)piles[i]/(double)mid);
+            }
+            if(cnt<=(long long)h){
+                ans=mid;
+                hi=mid-1;
             }
             else{
-                lo = mid + 1;
+                lo=mid+1;
             }
         }
-        return lo;
-
+        return ans;
     }
 };
