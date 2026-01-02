3e445db521813288a31b03d91e1af5ff3421508b
Pranshu0808 <pranshusharma789123@gmail.com>
2026-01-02 21:43:08 +0530

    Time: 41 ms (55.06%) | Memory: 85.8 MB (89.40%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums){
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int diff=0;
+
+        if(n<2){
+            return 0;
+            
+        }
+        for(int i=1;i<n;i++){
+            if(nums[i]-nums[i-1]>diff){
+                diff=nums[i]-nums[i-1];
+            }
+        }
+        return diff;
+    }
+};
