52322dfd434f9fca1227df42c5cb53d02526fd70
Pranshu0808 <pranshusharma789123@gmail.com>
2026-04-14 13:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (22.08%) - LeetSync

---
+++ 944-smallest-range-i/smallest-range-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) 
+    {
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+        
+        for (int x : nums) 
+        {
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
+        
+        int range = mx - mn - 2 * k;
+        return max(0, range);
+    }
+};
