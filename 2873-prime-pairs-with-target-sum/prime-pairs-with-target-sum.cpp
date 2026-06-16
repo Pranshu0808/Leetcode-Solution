7162068d9043af4d0c94927b4e48f33aeca4bc88
Pranshu0808 <pranshusharma789123@gmail.com>
2026-06-16 16:37:02 +0530

    Time: 68 ms (97.80%) | Memory: 35.4 MB (88.29%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
@@ -1,34 +1,68 @@
+// const int N = 1e6 + 1;
+// vector<int> isPrime(N, 1);
+
+// class Solution {
+// public:
+//     vector<vector<int>> findPrimePairs(int n) {
+//         isPrime[0] = isPrime[1] = false;
+
+//         for(int i = 2; 1LL * i * i < N; ++i){
+//             if(isPrime[i]){
+//                 for(int j = i * i; j < N; j += i){
+//                     isPrime[j] = false;
+//                 }
+//             }
+//         }
+
+//         vector<int> prime;
+//         for(int i = 2; i < N; ++i){
+//             if(isPrime[i]){
+//                 prime.push_back(i);
+//             }
+//         }
+
+//         vector<vector<int>> v;
+
+//         for(int i = 0; i < prime.size() && prime[i] <= n / 2; ++i){
+//             if(isPrime[n - prime[i]]){
+//                 v.push_back({prime[i], n - prime[i]});
+//             }
+//         }
+
+//         return v;
+//     }
+// };
+
 const int N = 1e6 + 1;
 vector<int> isPrime(N, 1);
 
 class Solution {
 public:
-    vector<vector<int>> findPrimePairs(int n) {
+    void sieve() {
+        if(isPrime[0] == 0) return;
+
         isPrime[0] = isPrime[1] = false;
 
-        for(int i = 2; 1LL * i * i < N; ++i){
-            if(isPrime[i]){
-                for(int j = i * i; j < N; j += i){
+        for(int i = 2; 1LL*i*i < N; ++i) {
+            if(isPrime[i]) {
+                for(int j = i*i; j < N; j += i) {
                     isPrime[j] = false;
                 }
             }
         }
+    }
 
-        vector<int> prime;
-        for(int i = 2; i < N; ++i){
-            if(isPrime[i]){
-                prime.push_back(i);
+    vector<vector<int>> findPrimePairs(int n) {
+        sieve();
+
+        vector<vector<int>> ans;
+
+        for(int i = 2; i <= n/2; ++i) {
+            if(isPrime[i] && isPrime[n-i]) {
+                ans.push_back({i, n-i});
             }
         }
 
-        vector<vector<int>> v;
-
-        for(int i = 0; i < prime.size() && prime[i] <= n / 2; ++i){
-            if(isPrime[n - prime[i]]){
-                v.push_back({prime[i], n - prime[i]});
-            }
-        }
-
-        return v;
+        return ans;
     }
 };
