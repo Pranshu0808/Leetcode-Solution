33687f9b44130d01a5e85c85b1085478f5716f9a
Pranshu0808 <pranshusharma789123@gmail.com>
2026-06-21 23:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.39%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,20 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ //                                RECURSIVE METHOD........
+// class Solution {
+// public:
+//     void dfs_pr(TreeNode* node , vector<int> &v){
+//         if(node == NULL){
+//             return;
+//         }
+//         v.push_back(node->val);
+//         dfs_pr(node->left , v);
+//         dfs_pr(node->right, v);
+
+//     }
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> v;
+//         dfs_pr(root , v);
+//         return v;
+//     }
+// };
+
+
+//                               ITERATIVE METHOD.........
 class Solution {
 public:
-    void dfs_pr(TreeNode* node , vector<int> &v){
-        if(node == NULL){
-            return;
+ vector<int> preorderTraversal(TreeNode* root){
+        vector<int> preorder;
+        if(root == NULL){
+            return preorder;
         }
-        v.push_back(node->val);
-        dfs_pr(node->left , v);
-        dfs_pr(node->right, v);
-
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> v;
-        dfs_pr(root , v);
-        return v;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != NULL){
+            st.push(root->right);
+            }
+            if(root->left != NULL){
+            st.push(root->left);
+            }
+        }
+        return preorder;
     }
 };
